785ea4956dca04b75dc16ba6e6dbd515d8a1a1f9
cuantico56 <37517897+cuantico56@users.noreply.github.com>
2024-09-24 21:25:19 -0400

    Update Inicio.py
    Agregado post

--- Inicio.py
+++ Inicio.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from pathlib import Path
+import aiofiles
 
 app = FastAPI()
 
@@ -14,3 +15,13 @@ def read_text_file():
         return {"Valor": content}
     except FileNotFoundError:
         raise HTTPException(status_code=404, detail="Archivo no encontrado")
+
+@app.post("/write")
+async def write_to_file(value: str):
+    """Escribe un valor al archivo Moneda.txt, sobrescribiendo el contenido existente."""
+    try:
+        async with aiofiles.open(file_path, 'w') as f:  # Utiliza aiofiles.open para abrir el archivo de forma asíncrona
+            await f.write(value)  # Escribe el valor
+        return {"message": "Valor escrito exitosamente."}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error al escribir: {str(e)}")
